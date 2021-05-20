630b26f512c9d26901c2bcb64dda326bfcd16441
Vu Khai Hoan <funny.khaihoan@gmail.com>
2021-05-20 19:47:45 +0700

    oke

--- auth.js
+++ auth.js
@@ -1,10 +1,8 @@
 import socket from "./socket.js";
-
+document.cookie = "cross-site-cookie1=nonCookie; SameSite=None";
 socket.on("connect", () => {
     console.log("connect");
 });
-const CookiesUD = Cookies.getJSON("user info");
-
 function submit() {
     const code = document.getElementById("verify-code").value;
     const nc = Number(code);
